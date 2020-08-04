a76476b6a7ac45622f27482342aa965ee00ba8ce
Owen Jones <owenthefirst93@gmail.com>
2020-08-03 22:56:09 -0400

    app file and reactrouter setup

--- js30react/src/App.js
+++ js30react/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class extends App {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className='App-header'>
+        </div>
+        <div className='App-body'>
+          <Switch>
+            <Route />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
